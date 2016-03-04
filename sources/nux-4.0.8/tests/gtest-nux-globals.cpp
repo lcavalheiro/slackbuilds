ac43d6004273fd953e3bbd1f30e0f66eaf8b1057
Luís Fernando Carvalho Cavalheiro <pessoal@profcavalheiro.com>
2016-03-04 11:54:46 -0300

    cleaned slackbuilds that aren't compiling

--- sources/nux-4.0.8/tests/gtest-nux-globals.cpp
+++
@@ -1,8 +0,0 @@
-#include <glib.h>
-
-gint64 g_fake_monotonic_time = 0;
-
-gint64 g_get_monotonic_time()
-{
-  return g_fake_monotonic_time;
-}
--- sources/nux-4.0.8/tests/gtest-nux-globals.h
+++
@@ -1,8 +0,0 @@
-#ifndef GTEST_NUX_GLOBALS_H
-#define GTEST_NUX_GLOBALS_H
-
-#include <glib.h>
-
-extern gint64 g_fake_monotonic_time;
-
-#endif // GTEST_NUX_GLOBALS_H
